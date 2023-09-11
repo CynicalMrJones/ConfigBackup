712a05a2aec8e09cecaef45abffbfc2dad6cc939
Juicy <juicy@pop-os.localdomain>
2023-09-11 18:35:25 -0400

    Added an auto installer for debian based systems

--- setup.lua
+++ setup.lua
@@ -6,6 +6,12 @@ local i3 = home .. "/.config/i3"
 local i3status = home .. "/.config/i3status"
 local polybar = home .. "/.config/polybar"
 
+
+local function installer()
+    os.execute("sudo apt update && sudo apt upgrade")
+    os.execute("sudo apt install awesome i3 polybar vim bashtop htop rofi ranger")
+end
+
 local function mkdir()
    os.execute("mkdir " .. alacritty)
    os.execute("mkdir " .. awesome)
@@ -23,10 +29,24 @@ local function copytime()
     os.execute("cp -v .bashrc " .. home .. "/")
 end
 
-
-local function setup()
-    mkdir()
-    copytime()
+local function menu()
+    os.execute("clear")
+    print("What would you like to do?")
+    print("1) Update .config files from ConfigBackup folder")
+    print("2) Run installer for Debian based systems")
+    print("3) Create the folders in .config and update them from latest ConfigBackup folder")
+    print("4) Exit Program")
+    local answer = io.read("*n")
+    if answer == 1 then
+        copytime()
+    elseif answer == 2 then
+        installer()
+    elseif answer == 3 then
+        mkdir()
+        copytime()
+    else
+        os.execute("exit")
+    end
 end
 
-setup()
+menu()
