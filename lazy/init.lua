1ae16fa02d4aa4367962b9a53f1781534126ba21
juicy <wopjr1234@gmail.com>
2026-01-19 21:17:40 -0500

    Updated Lazy

--- lazy/init.lua
+++ lazy/init.lua
@@ -22,6 +22,7 @@ vim.opt.termguicolors = true
 vim.opt.colorcolumn = "90"
 vim.opt.clipboard = "unnamedplus"
 vim.opt.signcolumn = 'yes'
+vim.opt.scrolloff= 8
 
 vim.cmd('colorscheme retrobox')
 
@@ -33,9 +34,37 @@ vim.lsp.config['clangd'] = {
     filetypes = { 'c', 'cpp' },
 }
 
+vim.lsp.config['jdtls'] = {
+    -- Command and arguments to start the server.
+    cmd = { 'jdtls' },
+    -- Filetypes to automatically attach to.
+    filetypes = { 'java' },
+}
+
 
 vim.lsp.enable('clangd')
 vim.lsp.enable('rust_analyzer')
+vim.lsp.enable('jdtls')
+
+-- This is where you enable features that only work
+-- if there is a language server active in the file
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = {buffer = event.buf}
+
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+  end,
+})
 
 --LSP stuff
 local cmp = require('cmp')
